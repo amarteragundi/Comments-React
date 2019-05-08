0efdb021bd04fdc907cedeed3dc74a1301e0c882
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 12:44:32 +0530

    refactoring to class

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
 
-    return(
-        <div></div>
-    )
+        
+        return(
+            <div>Latitude</div>
+        )
+    }
 }
 
 ReactDOM.render(
