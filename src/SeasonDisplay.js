64367c31324175febc9933bd44af372daf1cd203
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 22:43:36 +0530

    Removing latitude display from component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -5,13 +5,11 @@ const SeasonDisplay = props => {
   return (
     <div>
       <SnowStorm
-        flakesMax={1000}
         flakesMaxActive={400}
         animationInterval={50}
         vMaxY={10}
         vMaxX={10}
       />
-      <div className="seasons-bg">Latitude: {props.lat}</div>
     </div>
   );
 };
