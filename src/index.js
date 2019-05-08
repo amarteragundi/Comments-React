aaeec1bf42b678ac0bda4cacd4ced65917a39160
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 19:48:03 +0530

    adding get geolocation inside componentdidmount

--- src/index.js
+++ src/index.js
@@ -3,30 +3,26 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errMessage: "" };
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errMessage: "" };
+  }
 
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            err => this.setState({ errMessage: err.message })
-        );
-    }
-    render() {
-		if(this.state.errMessage && !this.state.lat)
-			return <div>error: {this.state.errMessage}</div>
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
+    );
+  }
 
-		if(this.state.lat)
-			return <div>Latitude : {this.state.lat}</div>
+  render() {
+    if (this.state.errMessage && !this.state.lat)
+      return <div>error: {this.state.errMessage}</div>;
 
-        return (
-            <div>
-               Loading ..
-            </div>
-        );
-    }
+    if (this.state.lat) return <div>Latitude : {this.state.lat}</div>;
+
+    return <div>Loading ..</div>;
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
