5512aeaa7cb18c334946ec7025b983b090db52e6
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 13:03:15 +0530

    adding initial state to the component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-class App extends React.Component{
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
 
-        
-        return(
-            <div>Latitude</div>
-        )
-    }
+    return <div>Latitude</div>;
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-)
+ReactDOM.render(<App />, document.querySelector("#root"));
