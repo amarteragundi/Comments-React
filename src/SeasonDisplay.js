344f6541e8c06010eba81651e2879a6cce598903
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 22:30:15 +0530

    adding bg and snowstorm

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
-import react from 'react';
+import React from "react";
+import SnowStorm from "react-snowstorm";
 
-const SeasonDisplay = () => {
-
-}
+const SeasonDisplay = props => {
+  return (
+    <div>
+      <SnowStorm
+        flakesMax={1000}
+        flakesMaxActive={400}
+        animationInterval={50}
+        vMaxY={10}
+        vMaxX={10}
+      />
+      <div className="seasons-bg">Latitude: {props.lat}</div>
+    </div>
+  );
+};
 
 export default SeasonDisplay;
