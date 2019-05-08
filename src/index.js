95cda05aea1bae3fe1e6eb8c1b6c097f397c7eee
Amar Teragundi <amarteragundi@gmail.com>
2019-05-08 22:29:51 +0530

    check if winter has come

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
   state = { lat: null, errMessage: "" };
-  componentDidMount() {
+
+  getLocation() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errMessage: err.message })
@@ -15,11 +16,16 @@ class App extends React.Component {
     if (this.state.errMessage && !this.state.lat)
       return <div>error: {this.state.errMessage}</div>;
 
-    if (this.state.lat) return(
-        <SeasonDisplay lat={this.state.lat} />
-    );
+    if (this.state.lat) return <SeasonDisplay lat={this.state.lat} />;
 
-    return <div>Loading ..</div>;
+    return (
+        <div className="bg">
+          <img onClick={this.getLocation.bind(this)}
+            style={{ "display":"block" ,position: "absolute", bottom: "60px", left: "20%", "cursor":"pointer" }}
+            src={process.env.PUBLIC_URL + "/has_winter_come.png"}
+          />
+        </div>
+      );
   }
 }
 
