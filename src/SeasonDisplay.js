74e85258954bd83bd195b8d287e5ce05ebb951e5
Amar Teragundi <amarteragundi@gmail.com>
2019-05-09 11:08:29 +0530

    get location and month and load season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,18 +1,33 @@
 import React from "react";
 import SnowStorm from "react-snowstorm";
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
 const SeasonDisplay = props => {
-  return (
-    <div>
-      <SnowStorm
-        flakesMaxActive={400}
-        animationInterval={50}
-        vMaxY={10}
-        vMaxX={10}
-      />
-      <div className="seasons-winter"></div>
-    </div>
-  );
+  const season = getSeason(props.lat, new Date().getMonth());
+  if(season == "winter"){
+    return (
+      <div>
+        <SnowStorm
+          flakesMaxActive={400}
+          animationInterval={50}
+          vMaxY={10}
+          vMaxX={10}
+        />
+        <div className="seasons-winter" />
+      </div>
+    );
+  } else {
+    return (
+      <div className="seasons-summer" />
+    )
+  }
 };
 
 export default SeasonDisplay;
