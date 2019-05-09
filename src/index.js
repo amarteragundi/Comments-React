26f88df740ff1c44b1f87bb925aac20e066f34a4
Amar Teragundi <amarteragundi@gmail.com>
2019-05-09 11:08:48 +0530

    show loader while fetching location

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  state = { lat: null, errMessage: "" };
+  state = { lat: null, errMessage: "", showLoader: false };
+
+  showLoader() {
+    this.setState({ showLoader: true });
+  }
+
+  hideLoader() {
+    this.setState({ showLoader: false });
+  }
 
   getLocation() {
+    this.showLoader();
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errMessage: err.message })
+      position => {
+        this.setState({ lat: position.coords.latitude });
+        this.hideLoader();
+      },
+      err => {
+        this.setState({ errMessage: err.message });
+        this.hideLoader();
+      }
     );
   }
 
@@ -18,14 +33,44 @@ class App extends React.Component {
 
     if (this.state.lat) return <SeasonDisplay lat={this.state.lat} />;
 
-    return (
-        <div className="bg">
-          <img onClick={this.getLocation.bind(this)}
-            style={{ "display":"block" ,position: "absolute", bottom: "60px", left: "20%", "cursor":"pointer" }}
-            src={process.env.PUBLIC_URL + "/has_winter_come.png"}
-          />
+    if (this.state.showLoader) {
+      return (
+        <div className="ui segment loading">
+          <div className="ui active dimmer">
+            <div
+              className="ui text loader"
+              style={{
+                fontSize: "30px",
+                display: "block",
+                position: "absolute",
+                bottom: "60px",
+                left: "20%",
+                cursor: "pointer",
+                textAlign: "center"
+              }}
+            >
+              Loading..
+            </div>
+          </div>
         </div>
       );
+    }
+
+    return (
+      <div className="bg">
+        <img
+          onClick={this.getLocation.bind(this)}
+          style={{
+            display: "block",
+            position: "absolute",
+            bottom: "60px",
+            left: "20%",
+            cursor: "pointer"
+          }}
+          src={process.env.PUBLIC_URL + "/has_winter_come.png"}
+        />
+      </div>
+    );
   }
 }
 
